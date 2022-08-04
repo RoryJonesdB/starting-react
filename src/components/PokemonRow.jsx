f09b92af6fe363c4c5212df7c413f63aac6300f5
RoryJonesdB <rory.jones@dbbroadcast.co.uk>
2022-08-04 08:54:23 +0100

    completed react 14 mobx

--- src/components/PokemonRow.jsx
+++ src/components/PokemonRow.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Button} from "@mui/material";
 
 
@@ -8,7 +7,7 @@ import PokemonType from "../pokemonType";
 
 const PokemonRow = ({ pokemon, onClick }) => (
   <>
-    <tr key={pokemon.id}>
+    <tr >
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ")}</td>
       <td>
@@ -25,7 +24,7 @@ const PokemonRow = ({ pokemon, onClick }) => (
 );
 
 PokemonRow.propTypes = {
-  pokemon: PropTypes.arrayOf(PokemonType),
+  pokemon: PokemonType,
 };
 
 export default PokemonRow
--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -1,27 +1,21 @@
 import React from "react"
-import useStore from "../store";
+import { observer } from "mobx-react";
+
+import store from "../store";
 
 import PokemonRow from "./PokemonRow"
 
 const PokemonTable = () => {
-    const pokemon = useStore((state)=>state.pokemon);
-    const filter = useStore((state)=>state.filter);
-    const setSelectedPokemon = useStore(state => state.setSelectedPokemon)
-
     return(
         <table width="100%">
             <tbody>
-                {pokemon
-                .filter(({ name: { english } }) =>
-                    english
-                    .toLocaleLowerCase()
-                    .includes(filter.toLocaleLowerCase())
-                )
+                {store.FilteredPokemon
                 .slice(0, 20)
                 .map((pokemon) => (
                     <PokemonRow
+                    key={pokemon.id}
                     pokemon={pokemon}
-                    onClick={(pokemon) => setSelectedPokemon(pokemon)}
+                    onClick={(pokemon) => store.setSelectedPokemon(pokemon)}
                     />
                 ))}
             </tbody>
@@ -30,4 +24,4 @@ const PokemonTable = () => {
 }
 
 
-export default PokemonTable
+export default observer(PokemonTable);
